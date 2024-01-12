6851774a99ed06d9fab834ff6c9151a9ed386205
Nonso Wogu <recreatesuccess@gmail.com>
2024-01-12 06:36:40 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/**
+ * Function to populate values in card from form input
+ */
+function generateCard() {
+    // Get value of Student name from form input 
+    const nameElement = document.getElementById("name");
+    const nameValue = nameElement.value;
+    
+    // Assign the value of student name to generated card
+    const cardNameElement = document.getElementById("cardName");
+    cardNameElement.innerHTML = nameValue;
+    
+    // Get value of college name from form input 
+    const collegeNameElement = document.getElementById("collegeName");
+    const collegeNameValue = collegeNameElement.value;
+    
+    // Assign the value of college name to generated card
+    document.getElementById("cardCollegeName").innerHTML = collegeNameValue;
+
+    // Get value of location name from form input 
+    const locationValue = document.getElementById("location").value;
+    
+    // Assign the value of location name to generated card
+    document.getElementById("cardLocation").innerHTML = locationValue;
+    
+    // Display final generated card to user     
+    document.getElementById("collegeCard").style.display = "block";
+}
